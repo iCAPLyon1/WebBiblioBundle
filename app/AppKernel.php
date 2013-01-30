fde3d1061851a0b221e977d08f4d515c12b4bcc1
SGWebCom <gaetan polliart>
2013-01-30 18:28:26 +0100

    Api Controller for Export

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new ICAP\Bundle\WebBiblioBundle\ICAPWebBiblioBundle(),
+            new IDCI\Bundle\ExporterBundle\IDCIExporterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
