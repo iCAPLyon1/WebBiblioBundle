19bf50bf487d4cecf96cce5625640520f4eb3f23
TSAVDARIS Panagiotis <ptsavdar@gmail.com>
2013-01-30 15:50:06 +0100

    Add Controller Actions

---
+++ src/ICAP/Bundle/WebBiblioBundle/Form/WebLinkType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ICAP\Bundle\WebBiblioBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class WebLinkType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('url', 'url')
+            ->add('username', 'email')
+            ->add('published')
+            ->add('tags', 'text')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ICAP\Bundle\WebBiblioBundle\Entity\WebLink'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'icap_bundle_webbibliobundle_weblinktype';
+    }
+}
